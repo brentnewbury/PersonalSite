b3e0f35c83e587841c80fcd3b90b271d0ef11167
Brent Newbury <brent@brentnewbury.com>
2015-07-28 21:42:08 +0100

    Upgraded to ASP.NET 5 beta 6

--- src/PersonalSite/Startup.cs
+++ src/PersonalSite/Startup.cs
@@ -43,7 +43,7 @@ namespace PersonalSite
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
-                app.UseErrorPage(ErrorPageOptions.ShowAll);
+                app.UseErrorPage();
             }
             else
             {
