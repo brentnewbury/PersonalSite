f045dff9b75320a9ffb377f267f2424b41cf21af
Brent Newbury <brent@brentnewbury.com>
2015-11-26 22:58:29 +0000

    Added remarks to CanonicalDomainMiddleware explaining why we don't just use RequireHttpsAttribute.

--- src/PersonalSite/Middleware/CanonicalDomainMiddleware.cs
+++ src/PersonalSite/Middleware/CanonicalDomainMiddleware.cs
@@ -8,6 +8,11 @@ namespace PersonalSite.Middleware
     /// <summary>
     /// Middleware component that redirects requests to specified domain, optionally requiring HTTPS.
     /// </summary>
+    /// <remarks>
+    /// We use this middleware rather than using <c>[RequireHtps]</c> as we don't want to potentially
+    /// have two successive 301 redirects should the incoming request be on a non-canonical domain and
+    /// not over HTTPS when required.
+    /// </remarks>
     public class CanonicalDomainMiddleware
     {
         private const string HttpScheme = "https";
