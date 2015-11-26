b2ccea74f421a7479801ec9a7a2ce625c8892876
Brent Newbury <brent@brentnewbury.com>
2015-11-26 22:54:46 +0000

    Added unit/integration tests for CanonicalDomainMiddleware.

---
+++ test/PersonalSite.Tests/CanonicalDomainMiddlewareTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using PersonalSite.Middleware;
+using Xunit;
+using System.Net;
+
+namespace PersonalSite.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class CanonicalDomainMiddlewareTests
+    {
+        [Fact]
+        public async Task RedirectToHttps()
+        {
+            var server = PersonalSiteTestServer.Create(app =>
+            {
+                app.UseCanonicalDomain(requireHttps: true);
+            });
+            var response = await server.CreateRequest("http://server/").GetAsync();
+
+            Assert.Equal(HttpStatusCode.MovedPermanently, response.StatusCode);
+            Assert.Equal("https", response.Headers.Location.Scheme);
+        }
+
+        [Fact]
+        public async Task RedirectToCanonicalDomain()
+        {
+            var canonicalDomain = "other.com";
+            var server = PersonalSiteTestServer.Create(app =>
+            {
+                app.UseCanonicalDomain(domain: canonicalDomain);
+            });
+            var response = await server.CreateRequest("http://server/").GetAsync();
+
+            Assert.Equal(HttpStatusCode.MovedPermanently, response.StatusCode);
+            Assert.Equal(response.Headers.Location.Host, canonicalDomain);
+        }
+
+        [Fact]
+        public async Task RedirectKeepsPathAndQueryIntact()
+        {
+            var canonicalDomain = "other.com";
+            var pathAndQuery = "path?query=value";
+            var server = PersonalSiteTestServer.Create(app =>
+            {
+                app.UseCanonicalDomain(domain: canonicalDomain);
+            });
+            var response = await server.CreateRequest($"http://wrong.com/{pathAndQuery}").GetAsync();
+
+            Assert.Equal(HttpStatusCode.MovedPermanently, response.StatusCode);
+            Assert.Equal(canonicalDomain, response.Headers.Location.Host);
+            Assert.Equal(pathAndQuery, response.Headers.Location.PathAndQuery.TrimStart('/'));
+        }
+    }
+}
---
+++ test/PersonalSite.Tests/PersonalSiteTestServer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalSite.Tests
+{
+    public static class PersonalSiteTestServer
+    {
+        public static TestServer Create(Action<IApplicationBuilder> configureApp)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddInMemoryCollection(new[]
+            {
+                new KeyValuePair<string, string>("webroot", ".")
+            });
+            return TestServer.Create(configurationBuilder.Build(), configureApp, configureServices: null);
+        }
+    }
+}
