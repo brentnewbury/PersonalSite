4bfa0058574c6c8742edb6985e2a87e2f511a23a
Brent Newbury <brent@brentnewbury.com>
2018-12-04 20:59:56 +0000

    Updated to ASP.NET Core 2.2

--- src/PersonalSite/Startup.cs
+++ src/PersonalSite/Startup.cs
@@ -25,7 +25,7 @@ namespace PersonalSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
